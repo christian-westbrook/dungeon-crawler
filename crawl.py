ba2a65837469d253a62e5ee10011818335467063
christian-westbrook <christianwestbrook@live.com>
2024-01-23 00:23:32 -0600

    Added loop for player commands, including new look command

--- crawl.py
+++ crawl.py
@@ -1,9 +1,25 @@
 from denizen.player import Player
 from dungeon.dungeon import Dungeon
 
+print('Welcome to Dungeon Crawler')
+
+# Initialize environment
 dungeon = Dungeon()
+current_room = dungeon.get_starting_point()
+
+# Initialize player
 player = Player(10)
 
-starting_point = dungeon.get_starting_point()
+playing = True
 
-print('You\'re standing in ' + starting_point.look())
+print('You\'re standing in ' + current_room.look())
+
+while playing:
+    command = input('--> ')
+
+    if command == 'look':
+        print('You\'re standing in ' + current_room.look())
+    if command == 'exit':
+        print('Exiting Dungeon Crawler')
+        print('Thanks for playing!')
+        exit()
